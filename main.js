5aedc4f3d5f0dcfb5feb70c6fcc62297b8305971
kishan bhakta <kishan.d.bhakta@gmail.com>
2015-05-11 14:32:35 -0400

    still not working

---
+++ main.js
@@ -0,0 +1,49 @@
+var givenName;
+// Q: What does `givenName` equal right now?
+    // givenName is declared as as undefined.
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+    // givenName is set up as a string with the assigned value of 'Tim'.
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+    // givenName is a variable assigned to 'JD'
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+    // greeting is assign the value of 'Hello, how are you?'
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// math = high - low 
+   math = 50 - 10;
+   math = 40
+
+math = high - "5";
+// Q: What is `math` set to?
+// math = high - '5' 
+   math = high - '5';
+   math = 45
+
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+
+var age = today - born;
+var timsAge = 'Tim is ' + age + ' years old' 
+
+
+
+
+/// Store some information following in variables.
+var yourName = 'Kishan Bhakta';
+var instructorName = 'Tim Whitacre';
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
